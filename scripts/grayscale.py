5c8867d53a574953ce9e8aa1e68a08523a3fbcdf
Xtalism <manuel_lego1234@hotmail.com>
2025-08-23 13:11:33 -0600

    Opencv -> Ros, image conversion

--- scripts/grayscale.py
+++ scripts/grayscale.py
@@ -2,15 +2,9 @@ import cv2
 import numpy as np
 from numpy.typing import NDArray
 
-
-def grayscale(path) -> NDArray[np.uint8]:
-    img = cv2.imread(path)
-    gray: NDArray[np.uint8] = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    return gray
-
-
 if __name__ == "__main__":
-    gray = grayscale("pictures/kanye.jpg")
+    img = cv2.imread("pictures/kanye.jpg")
+    gray: NDArray[np.uint8] = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     cv2.namedWindow("image", cv2.WINDOW_NORMAL | cv2.WINDOW_GUI_NORMAL)
     cv2.imshow(winname="image", mat=gray)
